0885eb87856178088b94c802b09f0061b9d995cb
BOUTHEINA RJAB <boutheinarjab302@gmail.com>
2026-04-22 11:26:55 +0100

    partie formations

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,13 @@
 import logo from './logo.png'
 import Couv from './Couv.jpg'
 import logo2 from './logo2.png'
+import React from './React.jpg'
 
 
 export const assets ={
     logo,
     logo2,
+    React,
     couverture: Couv,
 
 
